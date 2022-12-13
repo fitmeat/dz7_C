36b86a1125588e1a38ea300dd6764294ae0a812d
fitmeat <eenot825@gmail.com>
2022-12-13 18:44:26 +0300

    Выполнил задание 47

---
+++ task47/Program.cs
@@ -0,0 +1,18 @@
+﻿void CreateMatrix(double[,] matrix)
+{
+    for(int i = 0; i<matrix.GetLength(0); i++){
+        for(int j = 0; j<matrix.GetLength(1); j++){
+            matrix[i,j] = Math.Round(new Random().NextDouble() * (10+10) - 10, 1);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] arr = new double[m,n];
+CreateMatrix(arr);
